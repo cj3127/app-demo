02656db77243418281102a718ef5bfbaa0ad5081
瞬 <3127103271@qq.com>
2025-09-06 17:24:38 +0800

    Update User.java

--- src/main/java/com/demo/entity/User.java
+++ src/main/java/com/demo/entity/User.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor // 无参构造
 @AllArgsConstructor // 全参构造
 @Entity // 标记为 JPA 实体
-@Table(schema = "app_demo", name = "`user`") // 对应数据库表名：user
+@Table(schema = "app_demo", name = "user_test") // 对应数据库表名：user
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -43,6 +43,7 @@ public class User implements Serializable {
     }
 
 }
+
 
 
 
